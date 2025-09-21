2a69372538983ed3abdf4281740bd99109575185
Sandy Chow <sandychow@Sandys-MacBook-Pro.local>
2025-09-20 22:06:59 -0700

    add json file in data file, rename New function to Add

---
+++ .history/src/components/New_20250920215853.jsx
@@ -0,0 +1,7 @@
+function New () {
+  return (
+    <button className="new-button"> New </button>
+  )
+}
+
+export default New
---
+++ src/components/App.jsx
@@ -0,0 +1,44 @@
+import Book from "./Book";
+import Add from "./Add";
+
+function App() {
+  return (
+    <div>
+      <header className="app-header">
+       <h1 className="app-name">Book Catalog</h1>
+      </header>
+
+      <main className="app-main">
+        <div className="app-main-content">
+          <p className="app-main-content-text">Welcome to the Book Catalog! 📘  </p>
+          <p className="app-main-content-text">Feel free to click at <span className="learn-more">Learn More</span> to view detailed information of each book! 📖 </p>
+        </div>
+
+        <div className="app-content-container">
+            <Add />
+            <Book 
+            cover="book1.png"
+            alt="Cloud Native DevOps with Kubernetes" 
+            author="by Justin Domingus, John Arundel"
+            info="https://itbook.store/books/9781098116828"/>
+
+            <Book 
+            cover="book2.png"
+            alt="NGINX Cookbook" 
+            author="by Derek DeJonghe" 
+            info="https://itbook.store/books/9781098126247"/>
+          </div>
+      </main>
+
+      <footer className="app-footer">
+        <p>&copy; Sandy Chow, Set G, 2025</p>
+      </footer> 
+    </div>
+
+  )
+}
+
+export default App;
+
+
+
